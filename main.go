503b5c1b662f4a30f25247a4a446e3aad13949f2
wandergeek <innernetz@gmail.com>
2014-07-18 14:20:47 +1000

    first commit

---
+++ main.go
@@ -0,0 +1,312 @@
+package main
+
+import (
+	"./ninja"
+	"fmt"
+	"github.com/bcurren/go-hue"
+	"github.com/bitly/go-simplejson"
+	"log"
+	"math"
+	"os"
+	"os/signal"
+	"strings"
+	"time"
+)
+
+var _ = fmt.Printf
+
+type Light struct {
+	Id            string
+	Name          string
+	Bus           *ninja.DeviceBus
+	OnOffBus      *ninja.ChannelBus
+	colorBus      *ninja.ChannelBus
+	brightnessBus *ninja.ChannelBus
+	Bridge        *hue.Bridge
+	User          *hue.User
+	LightState    *hue.LightState
+}
+
+func (fl Light) SendOnOffState(state bool) error {
+
+	js, _ := simplejson.NewJson([]byte(`{
+    "state": ""
+  }`))
+
+	js.Set("state", state)
+
+	return fl.OnOffBus.SendEvent("state", js)
+
+}
+
+func getBridge() *hue.Bridge {
+	nobridge := true
+	var allbridges []*hue.Bridge
+	for nobridge {
+		allbridges, _ = hue.FindBridges()
+		if len(allbridges) == 0 {
+			log.Printf("Couldn't find bridge, retrying")
+			time.Sleep(time.Second * 2) //this sucks
+		} else {
+			nobridge = false
+			log.Printf("got %d bridges: %s", len(allbridges), allbridges)
+		}
+	}
+	return allbridges[0]
+}
+
+func getUser(bridge *hue.Bridge) *hue.User {
+	var user *hue.User
+	var err error
+	noUser := true
+	retries := 0
+	for noUser {
+		// user, err = bridge.CreateUser("ninjadevice", "ninjausername")
+		user, err = bridge.CreateUser("ninjadevice", "newdeveloper")
+
+		if err != nil {
+			if strings.Contains(err.Error(), "101") { // there's probably a nicer way to check this
+				retries++
+				log.Printf("Couldn't make user, push link button. Retry: %d", retries)
+				time.Sleep(time.Second * 2) //this sucks
+			} else {
+				log.Fatalf("Error creating user: %s", err)
+			}
+		}
+
+		if user != nil {
+			noUser = false
+		}
+	}
+	return user
+}
+
+func main() {
+
+	conn, err := ninja.Connect("10.0.1.171", 1883, "com.ninjablocks.hue") //TODO variable mqtt host and ID
+
+	bus, err := conn.AnnounceDriver("com.ninjablocks.hue", "driver-hue", getCurDir())
+	if err != nil {
+		log.Fatalf("Could not get driver bus: %s", err)
+	}
+
+	bridge := getBridge()
+	user := getUser(bridge)
+
+	allLights, err := user.GetLights()
+	if err != nil {
+		log.Fatalf("Couldn't get lights:  %s", err)
+	}
+
+	for _, l := range allLights {
+		_, err := NewLight(&l, bus, bridge, user)
+		if err != nil {
+			log.Fatalf("Error creating light instance:  %s", err)
+		}
+
+	}
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, os.Kill)
+
+	// Block until a signal is received.
+	s := <-c
+	fmt.Println("Got signal:", s)
+
+}
+
+func NewLight(l *hue.Light, bus *ninja.DriverBus, bridge *hue.Bridge, user *hue.User) (*Light, error) { //TODO cut this down!
+
+	lightState := createLightState()
+
+	light := &Light{
+		Id:         l.Id,
+		Name:       l.Name,
+		Bridge:     bridge,
+		User:       user,
+		LightState: lightState,
+	}
+
+	sigs, err := simplejson.NewJson([]byte(`{
+			"ninja:manufacturer": "Phillips",
+			"ninja:productName": "Hue",
+			"manufacturer:productModelId": "",
+			"ninja:productType": "Light",
+			"ninja:thingType": "light"
+	}`))
+
+	if err != nil {
+		log.Fatalf("Bad sig json: %s", err)
+	}
+
+	la, err := user.GetLightAttributes(l.Id)
+	if err != nil {
+		log.Fatalf("Bad sig json: %s", err)
+	}
+	sigs.Set("manufacturer:productModelId", la.ModelId)
+
+	log.Printf("Made light, id: %s, name: %s, signature: %s", l.Id, l.Name, sigs)
+
+	deviceBus, _ := bus.AnnounceDevice(l.Id, "hue", l.Name, sigs)
+	light.Bus = deviceBus
+
+	methods := "\"turnOn\", \"turnOff\", \"set\"" //TODO: should be string array
+	events := "\"state\""
+	onOffBus, _ := light.Bus.AnnounceChannel("on-off", "on-off", methods, events, func(method string, payload *simplejson.Json) {
+
+		switch method {
+		case "turnOn":
+			log.Printf("Turning on!")
+			light.turnOnOff(true)
+		case "turnOff":
+			log.Printf("Turning off!")
+			light.turnOnOff(false)
+		case "set":
+			state, _ := payload.GetIndex(0).Bool()
+			log.Printf("Setting to %t!", state)
+			light.turnOnOff(state)
+		default:
+			log.Printf("On-off got an unknown method %s", method)
+		}
+
+	})
+	light.OnOffBus = onOffBus
+
+	methods = "\"set\""
+	brightnessBus, _ := light.Bus.AnnounceChannel("brightness", "brightness", methods, events, func(method string, payload *simplejson.Json) {
+
+		switch method {
+		case "set":
+			brightness, _ := payload.GetIndex(0).Float64()
+			log.Printf("Setting brightness to %f!", brightness)
+			light.setBrightness(brightness)
+		default:
+			log.Printf("Brightness got an unknown method %s", method)
+		}
+
+	})
+	light.brightnessBus = brightnessBus
+
+	colorBus, _ := light.Bus.AnnounceChannel("color", "color", methods, events, func(method string, payload *simplejson.Json) {
+		log.Printf("Received actuation to change color, payload is: %s", payload)
+		mode, _ := payload.Get("mode").String()
+		log.Printf("Unsupported mode %s", mode)
+		switch method {
+		case "set":
+			log.Printf("Setting color to %s!", payload)
+			light.setColor(payload, method)
+		default:
+			log.Printf("Color got an unknown method %s", method)
+		}
+	})
+	light.colorBus = colorBus
+
+	return light, nil
+}
+
+func (l Light) turnOnOff(state bool) {
+	l.refreshLightState()
+	l.LightState.On = &state
+	l.User.SetLightState(l.Id, l.LightState)
+}
+
+func (l Light) setBrightness(fbrightness float64) {
+	brightness := uint8(fbrightness * math.MaxUint8)
+	on := bool(true)
+	l.LightState.Brightness = &brightness
+	l.LightState.On = &on
+	log.Printf("Setting brightness, fbrightness: %f, brightness: %d", fbrightness, brightness)
+	l.refreshLightState()
+	l.User.SetLightState(l.Id, l.LightState)
+
+}
+
+func (l Light) setColor(payload *simplejson.Json, mode string) {
+	switch mode {
+	case "hue": //less verbose plz
+		fhue, _ := payload.Get("hue").Float64()
+		hue := uint16(fhue * math.MaxUint16)
+		fsaturation, _ := payload.Get("saturation").Float64()
+		saturation := uint8(fsaturation * math.MaxUint8)
+		on := bool(true)
+		l.LightState.Hue = &hue
+		l.LightState.Saturation = &saturation
+		l.LightState.On = &on
+		log.Printf("Color from setColor, fhue: %f, fsaturation: %f, hue: %d, saturation: %d", fhue, fsaturation, hue, saturation)
+	case "xy":
+		x, _ := payload.Get("x").Float64()
+		y, _ := payload.Get("y").Float64()
+		xy := []float64{x, y}
+		l.LightState.XY = xy
+	case "temperature":
+		temp, _ := payload.Get("temperature").Float64()
+		utemp := uint16(math.Floor(1000000 / temp))
+		l.LightState.ColorTemp = &utemp
+	default:
+		log.Printf("Bad color mode: %s", mode)
+	}
+	l.refreshLightState()
+	l.User.SetLightState(l.Id, l.LightState)
+
+}
+
+func createLightState() *hue.LightState {
+
+	on := bool(false)
+	brightness := uint8(0)
+	saturation := uint8(0)
+	hueVal := uint16(0)
+	transitionTime := uint16(0)
+	alert := ""
+	temp := uint16(0)
+	xy := []float64{0, 0}
+
+	lightState := &hue.LightState{
+		On:             &on,
+		Brightness:     &brightness,
+		Saturation:     &saturation,
+		Hue:            &hueVal,
+		ColorTemp:      &temp,
+		TransitionTime: &transitionTime,
+		Alert:          alert,
+		XY:             xy,
+	}
+
+	return lightState
+}
+
+func getCurDir() string {
+	pwd, err := os.Getwd()
+	check(err)
+	return pwd + "/"
+}
+
+func check(e error) {
+	log.Printf("boom")
+	if e != nil {
+		panic(e)
+	}
+}
+
+func (l Light) refreshLightState() {
+	newstate, _ := l.User.GetLightAttributes(l.Id)
+	bulbstate := newstate.State
+	mybulbstate := l.LightState
+	transtime := uint16(5) //TODO: Remove when web is sending sensible value
+	// l.LightState = newstate.State //TODO why doesnt this work?
+
+	mybulbstate.On = bulbstate.On
+	mybulbstate.Brightness = bulbstate.Brightness
+	mybulbstate.Hue = bulbstate.Hue
+	mybulbstate.Saturation = bulbstate.Saturation
+	mybulbstate.XY = bulbstate.XY
+	mybulbstate.ColorTemp = bulbstate.ColorTemp
+	mybulbstate.Alert = bulbstate.Alert
+	mybulbstate.Effect = bulbstate.Effect
+	mybulbstate.TransitionTime = &transtime
+}
+
+func printState(s *hue.LightState) {
+	log.Printf(" on:%t brightness: %d hue: %d sat: %d x,y: %f, %f colortemp: %d alert: %s effect: %s color mode: %s reachable: %t", *s.On, *s.Brightness, *s.Hue, *s.Saturation, s.XY[0], s.XY[1], *s.ColorTemp, s.Alert, s.Effect, s.ColorMode, s.Reachable)
+
+}
---
+++ ninja/ninja.go
@@ -0,0 +1,282 @@
+package ninja
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"fmt"
+	MQTT "git.eclipse.org/gitroot/paho/org.eclipse.paho.mqtt.golang.git"
+	"github.com/bitly/go-simplejson"
+	"io/ioutil"
+	"log"
+	"time"
+)
+
+func Connect(host string, port int, clientId string) (*NinjaConnection, error) {
+	fmt.Sprintf("Connecting to mqtt server %s:%d", host, port)
+
+	mqttServer := fmt.Sprintf("tcp://%s:%d", host, port)
+	conn := NinjaConnection{}
+
+	opts := MQTT.NewClientOptions().SetBroker(mqttServer).SetClientId(clientId).SetCleanSession(true).SetTraceLevel(MQTT.Off)
+	conn.mqtt = MQTT.NewClient(opts)
+	_, err := conn.mqtt.Start()
+	if err != nil {
+		log.Fatalf("Failed to connect to mqtt server %s - %s", host, err)
+	} else {
+		log.Printf("Connected to %s\n", host)
+	}
+
+	return &conn, nil
+}
+
+type NinjaConnection struct {
+	mqtt *MQTT.MqttClient
+}
+
+func (n NinjaConnection) AnnounceDriver(id string, name string, path string) (*DriverBus, error) {
+	js, err := simplejson.NewJson([]byte(`{
+		"params": [
+		{
+	    "name": "",
+	    "file": "",
+			"defaultConfig" : {},
+			"package": {}
+		}],
+		"time":"",
+		"jsonrpc":"2.0"
+  }`))
+
+	if err != nil {
+		log.Fatalf("Bad json: %s", err)
+	}
+
+	pkginfo := getDriverInfo(path + "package.json")
+	tmp, err := pkginfo.Get("main").String()
+	if err != nil {
+		log.Fatalf("Couldn't retrieve main filename: %s", err)
+	}
+
+	mainfile := path + tmp
+	js.Get("params").GetIndex(0).Set("file", mainfile)
+	js.Get("params").GetIndex(0).Set("name", id)
+	js.Get("params").GetIndex(0).Set("package", pkginfo)
+	js.Get("params").GetIndex(0).Set("defaultConfig", "{}") //TODO fill me out
+	js.Set("time", time.Now().Unix())
+	json, _ := js.MarshalJSON()
+
+	serial := "TEST" //TODO: get config
+	version, err := pkginfo.Get("version").String()
+	if err != nil {
+		log.Fatalf("No version available for driver %s: %s", id, err)
+	}
+
+	receipt := n.mqtt.Publish(MQTT.QoS(1), "$node/"+serial+"/app/"+id+"/event/announce", json)
+	<-receipt
+
+	driverBus := &DriverBus{
+		id:      id,
+		name:    name,
+		mqtt:    n.mqtt,
+		version: version,
+	}
+
+	return driverBus, nil
+}
+
+type DriverBus struct {
+	id      string
+	name    string
+	version string
+	mqtt    *MQTT.MqttClient
+}
+
+func (d DriverBus) AnnounceDevice(id string, idType string, name string, sigs *simplejson.Json) (*DeviceBus, error) {
+	js, err := simplejson.NewJson([]byte(`{
+    "params": [
+        {
+            "guid": "",
+            "id": "",
+            "idType": "",
+            "name": "",
+            "signatures": {},
+            "driver": {
+                "name": "",
+                "version": ""
+            }
+        }
+    ],
+    "time": "",
+    "jsonrpc": "2.0"
+}`))
+
+	if err != nil {
+		log.Fatalf("Bad driver announce JSON: %s", js)
+	}
+
+	guid := GetGUID(d.id + id)
+	js.Get("params").GetIndex(0).Set("guid", guid)
+	js.Get("params").GetIndex(0).Set("id", id) //TODO patch driver to get MAC ID, rather than numberical ID
+	js.Get("params").GetIndex(0).Set("idType", idType)
+	js.Get("params").GetIndex(0).Set("name", name)
+	js.Get("params").GetIndex(0).Set("signatures", sigs)
+	js.Get("params").GetIndex(0).Get("driver").Set("name", d.name)
+	js.Get("params").GetIndex(0).Get("driver").Set("version", d.version)
+	js.Set("time", time.Now().Unix())
+
+	json, err := js.MarshalJSON()
+	if err != nil {
+		log.Fatalf("Couldn't stringify: %s", err)
+	}
+
+	receipt := d.mqtt.Publish(MQTT.QoS(1), "$device/"+guid+"/announce/", json)
+	// log.Printf("  message sent!")
+	<-receipt
+	// log.Printf("  message delivered!")
+
+	deviceBus := &DeviceBus{
+		id:         id,
+		idType:     idType,
+		name:       name,
+		driver:     &d,
+		devicejson: js.Get("params").GetIndex(0),
+	}
+
+	return deviceBus, nil
+}
+
+type DeviceBus struct {
+	id         string
+	idType     string
+	name       string
+	driver     *DriverBus
+	devicejson *simplejson.Json
+}
+
+type JsonMessageHandler func(string, *simplejson.Json)
+
+func (d DeviceBus) AnnounceChannel(name string, protocol string, methods string, events string, serviceCallback JsonMessageHandler) (*ChannelBus, error) {
+	deviceguid, _ := d.devicejson.Get("guid").String()
+	channelguid := GetGUID(name + protocol)
+	js, _ := simplejson.NewJson([]byte(`{
+    "params": [
+        	{
+            "channel": "",
+            "supported": {
+                "methods": [],
+                "events": []
+            },
+            "device": {}
+        }
+    ],
+    "time": "",
+    "jsonrpc": "2.0"
+}`))
+
+	js.Get("params").GetIndex(0).Set("device", d.devicejson)
+	methodsjson := strArrayToJson(methods)
+	js.Get("params").GetIndex(0).Get("supported").Set("methods", methodsjson)
+	eventsjson := strArrayToJson(events)
+	js.Get("params").GetIndex(0).Get("supported").Set("events", eventsjson)
+	js.Get("params").GetIndex(0).Set("channel", name)
+	js.Set("time", time.Now().Unix())
+
+	json, err := js.MarshalJSON()
+
+	if err != nil {
+		log.Fatalf("Couldn't stringify that message %s", err)
+	}
+
+	topicBase := "$device/" + deviceguid + "/channel/" + channelguid + "/" + protocol
+
+	pubReceipt := d.driver.mqtt.Publish(MQTT.QoS(0), topicBase+"/announce", json)
+	<-pubReceipt
+
+	log.Printf("Subscribing to : %s", topicBase)
+	filter, err := MQTT.NewTopicFilter(topicBase, 0)
+	if err != nil {
+		log.Fatalf("unable to subscribe to %s in announcechannel: %s", topicBase, err)
+	}
+	_, err = d.driver.mqtt.StartSubscription(func(client *MQTT.MqttClient, message MQTT.Message) {
+		json, _ := simplejson.NewJson(message.Payload())
+		method, _ := json.Get("method").String()
+		params := json.Get("params")
+
+		log.Printf("incoming message! %s", params)
+		serviceCallback(method, params)
+
+	}, filter)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	channelBus := &ChannelBus{
+		name:     name,
+		protocol: protocol,
+		device:   &d,
+	}
+
+	return channelBus, nil
+}
+
+func (cb ChannelBus) SendEvent(event string, payload *simplejson.Json) error {
+	json, err := payload.MarshalJSON()
+	if err != nil {
+		return err
+	}
+
+	receipt := cb.device.driver.mqtt.Publish(MQTT.QoS(0), "$driver/"+cb.device.driver.id+"/device/"+cb.device.id+"/channel/"+cb.name+"/"+cb.protocol+"/event/"+event, json)
+	<-receipt
+
+	return nil
+}
+
+type ChannelBus struct {
+	name     string
+	protocol string
+	device   *DeviceBus
+	channel  <-chan MQTT.Receipt
+}
+
+func GetConnection() (*NinjaConnection, error) {
+	host := "localhost"
+	port := 1883
+	clientId := "mqttClientId"
+
+	return Connect(host, port, clientId)
+}
+
+func getDriverInfo(filename string) (res *simplejson.Json) {
+	dat, err := ioutil.ReadFile(filename)
+	check(err)
+	js, err := simplejson.NewJson(dat)
+	check(err)
+	js.Del("scripts")
+	log.Printf("driver info: %s", res)
+	return js
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func GetGUID(in string) string {
+
+	h := md5.New()
+	h.Write([]byte(in))
+	str := hex.EncodeToString(h.Sum(nil))
+	return str[:10]
+
+}
+
+func strArrayToJson(in string) *simplejson.Json {
+	str := []byte("[" + in + "]")
+	out, err := simplejson.NewJson(str)
+	if err != nil {
+		log.Fatalf("Bad JSON in strArrayToJson %+v: %s", in, err)
+	}
+
+	return out
+}
