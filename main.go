b738dda0359c00057014b3f479c541f33d70419f
Elliot Shepherd <elliot@ninjablocks.com>
2014-10-01 14:35:08 +1000

    Switch to new api

--- main.go
+++ main.go
@@ -4,54 +4,11 @@ import (
 	"fmt"
 	"os"
 	"os/signal"
-
-	"github.com/ninjasphere/go-ninja"
-	"github.com/ninjasphere/go-ninja/logger"
 )
-
-const driverName = "driver-hue"
-
-var log = logger.GetLogger(driverName)
 
 func main() {
 
-	log.Infof("Starting " + driverName)
-
-	conn, err := ninja.Connect("com.ninjablocks.hue")
-	if err != nil {
-		log.HandleError(err, "Could not connect to MQTT")
-	}
-
-	pwd, _ := os.Getwd()
-
-	bus, err := conn.AnnounceDriver("com.ninjablocks.hue", driverName, pwd)
-	if err != nil {
-		log.HandleError(err, "Could not get driver bus")
-	}
-
-	statusJob, err := ninja.CreateStatusJob(conn, driverName)
-
-	if err != nil {
-		log.HandleError(err, "Could not setup status job")
-	}
-
-	statusJob.Start()
-
-	bridge := getBridge()
-	user := getUser(bridge)
-
-	allLights, err := user.GetLights()
-	if err != nil {
-		log.HandleError(err, "Couldn't get lights")
-	}
-
-	for _, l := range allLights {
-		_, err := NewLight(&l, bus, bridge, user)
-		if err != nil {
-			log.HandleError(err, "Error creating light instance")
-		}
-
-	}
+	NewHueDriver()
 
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt, os.Kill)
